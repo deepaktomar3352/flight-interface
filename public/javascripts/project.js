bf25a9918e82b9667f8509c00e307aaa60d143d4
Deepak Singh Tomar <112108549+deepaktomar3352@users.noreply.github.com>
2023-06-10 12:12:54 +0530

    Update project.js

--- public/javascripts/project.js
+++ public/javascripts/project.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $.getJSON("http://localhost:3000/flight/fetchallcities",function(data){
+    $.getJSON("https://flight-interface.onrender.com/fetchallcities",function(data){
         // alert(JSON.stringify(data))
         data.result.map((item)=>{
             $('#source').append($('<option>').text(item.cityname))
